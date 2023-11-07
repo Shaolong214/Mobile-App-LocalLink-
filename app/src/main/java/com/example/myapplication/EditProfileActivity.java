a3e16f441a84f39f526ef8a57b1b11e6171c6c5d
Shaolong214 <103941617+Shaolong214@users.noreply.github.com>
2023-11-07 20:41:17 +1100

    edit profile image

--- app/src/main/java/com/example/myapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/myapplication/EditProfileActivity.java
@@ -1,16 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -22,6 +30,11 @@ import java.util.HashMap;
 import javax.annotation.Nullable;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
+import android.app.Activity;
 
 public class EditProfileActivity extends AppCompatActivity {
     private EditText userName, userStatus;
@@ -30,6 +43,8 @@ public class EditProfileActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private String currentUserId;
+    private DocumentReference imageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +53,7 @@ public class EditProfileActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
+
 
         userName = (EditText) findViewById(R.id.edit_user_name);
         userStatus = (EditText) findViewById(R.id.edit_profile_status);
@@ -62,7 +78,60 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+        imageRef = db.collection("images").document(currentUserId);
+
+        userProfImage.setOnClickListener(view -> {
+            Intent galleryIntent = new Intent();
+            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+            galleryIntent.setType("image/*");
+            resultLauncher.launch(galleryIntent);
+        });
+
+        imageRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@androidx.annotation.Nullable DocumentSnapshot value, @androidx.annotation.Nullable FirebaseFirestoreException error) {
+                if(value!=null){
+                    if(value.getData()!=null) {
+                        String image = value.getData().get("uri").toString();
+                        Picasso.get().load(image).placeholder(R.drawable.profile).into(userProfImage);
+                    }
+                }
+            }
+        });
+
     }
+
+    ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData()!=null) {
+                        Intent data = result.getData();
+                        String imageUri = data.getData().toString();
+
+                        HashMap userImage = new HashMap();
+                        userImage.put("uri", imageUri);
+
+                        imageRef.set(userImage).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Intent selfIntent = new Intent(EditProfileActivity.this, EditProfileActivity.class);
+                                    startActivity(selfIntent);
+                                } else {
+                                    Toast.makeText(EditProfileActivity.this, "Error adding photo", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+
+
+
+
 
     private void ValidateAccountInfo() {
         String username = userName.getText().toString();
@@ -101,7 +170,6 @@ public class EditProfileActivity extends AppCompatActivity {
                 });
     }
 
-
     private void retrieveAndDisplayUserDetails() {
         // Fetch user details
         db.collection("users").document(currentUserId)
@@ -126,23 +194,44 @@ public class EditProfileActivity extends AppCompatActivity {
                 });
 
         // Fetch profile image
-        db.collection("images").document(currentUserId)
-                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
-                                        @Nullable FirebaseFirestoreException e) {
-                        if (e != null) {
-                            // Handle the exception
-                            return;
-                        }
+//        db.collection("images").document(currentUserId)
+//                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+//                                        @Nullable FirebaseFirestoreException e) {
+//                        if (e != null) {
+//                            // Handle the exception
+//                            return;
+//                        }
+//
+//                        if (documentSnapshot != null && documentSnapshot.exists()) {
+//                            // Fetch profile image
+//                            String myProfileImage = documentSnapshot.getString("uri");
+//                            if (myProfileImage != null && !myProfileImage.isEmpty()) {
+//                                Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfImage);
+//                            }
+//                        }
+//                    }
+//                });
 
-                        if (documentSnapshot != null && documentSnapshot.exists()) {
+        db.collection("images").document(currentUserId)
+                .get() // Changed to get() for a single read instead of a real-time listener
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
                             // Fetch profile image
                             String myProfileImage = documentSnapshot.getString("uri");
                             if (myProfileImage != null && !myProfileImage.isEmpty()) {
                                 Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfImage);
                             }
                         }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ProfileActivity", "Error fetching profile image", e);
                     }
                 });
     }
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,146 +1,3 @@
-//package com.example.myapplication;
-//
-//import androidx.annotation.NonNull;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.annotation.SuppressLint;
-//import android.os.Bundle;
-//import android.provider.Settings;
-//import android.widget.EditText;
-//import android.widget.TextView;
-//
-//import com.example.myapplication.R;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.squareup.picasso.Picasso;
-//
-//import de.hdodenhof.circleimageview.CircleImageView;
-//
-//public class ProfileActivity extends AppCompatActivity {
-//    private TextView userName, userStatus, userConstantName;
-//    private CircleImageView userProfileImage;
-//
-//    private DatabaseReference profileUserRef;
-//    private FirebaseAuth mAuth;
-//    private String currentUserId;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_profile);
-//
-//        mAuth = FirebaseAuth.getInstance();
-//        currentUserId = mAuth.getCurrentUser().getUid();
-//        profileUserRef = FirebaseDatabase.getInstance().getReference().child("users").child(currentUserId);
-//
-//        userName = findViewById(R.id.my_user_name);
-//        userConstantName = findViewById(R.id.my_name);
-//        userStatus = findViewById(R.id.my_profile_status);
-//        userProfileImage = findViewById(R.id.my_profile_pic);
-//
-//        profileUserRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                if (dataSnapshot.exists()) {
-//                    String myProfileImage = dataSnapshot.child("profileimage").getValue(String.class);
-//                    String myUserName = dataSnapshot.child("username").getValue(String.class);
-//                    String myName = dataSnapshot.child("name").getValue(String.class);
-//                    String myProfileStatus = dataSnapshot.child("status").getValue(String.class);
-//
-//                    if (myProfileImage != null) {
-//                        Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
-//                    }
-//                    userName.setText("@" + myUserName);
-//                    userConstantName.setText(myName);
-//                    userStatus.setText(myProfileStatus);
-//                }
-//            }
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                // Handle possible errors.
-//            }
-//        });
-//    }
-//}
-
-
-/* above v1, below v2 */
-
-
-//package com.example.myapplication;
-//
-//import androidx.annotation.NonNull;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//import android.widget.TextView;
-//
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.firestore.DocumentSnapshot;
-//import com.google.firebase.firestore.EventListener;
-//import com.google.firebase.firestore.FirebaseFirestore;
-//import com.google.firebase.firestore.FirebaseFirestoreException;
-//import com.squareup.picasso.Picasso;
-//
-//import javax.annotation.Nullable;
-//
-//import de.hdodenhof.circleimageview.CircleImageView;
-//
-//public class ProfileActivity extends AppCompatActivity {
-//    private TextView userName, userStatus, userConstantName;
-//    private CircleImageView userProfileImage;
-//
-//    private FirebaseFirestore db;
-//    private FirebaseAuth mAuth;
-//    private String currentUserId;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_profile);
-//
-//        mAuth = FirebaseAuth.getInstance();
-//        currentUserId = mAuth.getCurrentUser().getUid();
-//        db = FirebaseFirestore.getInstance();
-//
-//        userName = findViewById(R.id.my_user_name);
-//        userConstantName = findViewById(R.id.my_name);
-//        userStatus = findViewById(R.id.my_profile_status);
-//        userProfileImage = findViewById(R.id.my_profile_pic);
-//
-//        db.collection("users").document(currentUserId).addSnapshotListener(new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot dataSnapshot, @Nullable FirebaseFirestoreException e) {
-//                if (e != null) {
-//                    // Handle error
-//                    // ...
-//                    return;
-//                }
-//
-//                if (dataSnapshot != null && dataSnapshot.exists()) {
-//                    String myProfileImage = dataSnapshot.getString("profileimage");
-//                    String myUserName = dataSnapshot.getString("username");
-//                    String myName = dataSnapshot.getString("name");
-//                    String myProfileStatus = dataSnapshot.getString("status");
-//
-//                    if (myProfileImage != null && !myProfileImage.isEmpty()) {
-//                        Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
-//                    }
-//                    userName.setText("@" + myUserName);
-//                    userConstantName.setText(myName);
-//                    userStatus.setText(myProfileStatus);
-//                }
-//            }
-//        });
-//    }
-//}
-
-/* above v2, below v3 */
-
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
@@ -148,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -205,6 +65,12 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        retrieveAndDisplayUserDetails();
+    }
+
     private void retrieveAndDisplayUserDetails() {
         // Fetch user details
         db.collection("users").document(currentUserId)
@@ -231,23 +97,45 @@ public class ProfileActivity extends AppCompatActivity {
                 });
 
         // Fetch profile image
-        db.collection("images").document(currentUserId)
-                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
-                                        @Nullable FirebaseFirestoreException e) {
-                        if (e != null) {
-                            // Handle the exception
-                            return;
-                        }
+//        db.collection("images").document(currentUserId)
+//                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+//                                        @Nullable FirebaseFirestoreException e) {
+//                        if (e != null) {
+//                            // Handle the exception
+//                            return;
+//                        }
+//
+//                        if (documentSnapshot != null && documentSnapshot.exists()) {
+//                            // Fetch profile image
+//                            String myProfileImage = documentSnapshot.getString("uri");
+//                            if (myProfileImage != null && !myProfileImage.isEmpty()) {
+//                                Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
+//                            }
+//                        }
+//                    }
+//                });
 
-                        if (documentSnapshot != null && documentSnapshot.exists()) {
+        db.collection("images").document(currentUserId)
+                .get() // Changed to get() for a single read instead of a real-time listener
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
                             // Fetch profile image
                             String myProfileImage = documentSnapshot.getString("uri");
                             if (myProfileImage != null && !myProfileImage.isEmpty()) {
                                 Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
+
                             }
                         }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ProfileActivity", "Error fetching profile image", e);
                     }
                 });
     }
@@ -258,5 +146,3 @@ public class ProfileActivity extends AppCompatActivity {
     }
 }
 
-// implement status
-// implement edit
