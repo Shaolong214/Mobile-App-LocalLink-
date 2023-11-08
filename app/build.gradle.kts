e86ea17a81b787acbb6723350b5c6fe0d885f541
liqinzhang98 <liqizhang@student.unimelb.edu.au>
2023-11-08 13:38:53 +1100

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 
     implementation("com.squareup.picasso:picasso:2.8")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
 }
 // added to resolve the duplicate classes issue
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -101,7 +101,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // map
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
         getLastLocation();
 
     }
